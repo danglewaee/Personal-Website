b4dc6fdaf34f42f8ea2c07cb8e343f2a33e52f39
danglewaee <lehaidang0902006@gmail.com>
2026-03-18 23:36:52 -0400

    Restore skills section on projects page

--- script.js
+++ script.js
@@ -1227,6 +1227,7 @@ function renderAboutPage() {
 function renderWorkPage() {
   renderProjectGallery("project-gallery", caseStudyProjects);
   renderArchiveProjectLinks("archive-project-links", archiveProjects);
+  renderSkillGroups("work-skill-groups");
 }
 
 function renderExperiencePage() {
